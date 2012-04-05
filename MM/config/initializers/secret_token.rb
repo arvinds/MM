96798597a4afd707dc1f960a611385a1d8514588
Arvind Shrihar <arvind.shrihari@live.com>
2012-04-04 20:43:00 -0700

    init project

---
+++ MM/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MM::Application.config.secret_token = 'bd64f8ce821628ae5d3aec4cc393df53ba70e97c4a1e98420d677ee15736c6d8424de6f8e777de6a1fab7d23a7822654f1149ecaea3f2faf4baffdbe4a775f6d'
