da088775e6c11f9f8fd0c9d6300c813b18846141
S P <zenithwolf@hotmail.com>
2012-05-21 23:14:47 -0700

    * Added basic bing map

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+	var map = null;
+
 	// Body click bindings
 	$('body').on('click', function(e) {
 	
@@ -8,6 +10,11 @@ $(document).ready(function() {
 			&& !($('#sign_in_dropdown').hasClass('hidden'))
 			&& ($(e.target).closest('#sign_in_dropdown').length == 0)) {
 			$('#sign_in').trigger('click');
+		}
+		
+		if (($(e.target).closest('#map_container').length == 0) &&
+			!($('#to_from_container').find('input').is(':focus'))) {
+			$('#map_container').fadeOut('slow').addClass('hidden');
 		}
 	});
 
@@ -26,5 +33,29 @@ $(document).ready(function() {
 			$('#sign_in_dropdown').stop().fadeOut('fast').addClass('hidden');
 		}
 	});
-
+	
+	$('#from_select').on('focus', function(event) {
+		var $this = $(this);
+		var top = $this.offset().top;
+		var left = $this.offset().left;
+		if (!map) {
+			loadMap();
+		}
+		$('#map_container').removeClass('hidden').css({
+			top : top + $this.outerHeight() + "px",
+			left : left
+		}).fadeIn('slow');
+	});
+	
+	var loadMap = function() {
+		var mapOptions = {
+			credentials:"	Aq1YIJbPm7C4HzFc_271JbEI8BoEIoccyB_g10Fa21jupGJKBG-Ocv0Cbmbt0cet",
+			disableBirdseye : true,
+			showDashboard : false,
+			mapTypeId: Microsoft.Maps.MapTypeId.road
+		};
+		map = new Microsoft.Maps.Map(document.getElementById("map_container"), mapOptions);
+	}
+	
+	
 });
