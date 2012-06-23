a84d7fb125726c463a82705ab7ac95ca6303fa10
Arvind Shrihar <arvind.shrihari@live.com>
2012-06-23 16:10:30 -0700

    udoing last change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,6 @@ MM::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  ActiveMerchant::Billing::Base.mode = :test
+  #ActiveMerchant::Billing::Base.mode = :test
 
 end
